6abeabd38910f2370ade916f501d1dee8445b14a
DSARRAZY <d.sarrazy@gmail.com>
2020-11-06 17:15:39 +0100

    mouseover

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 
 export default class MovieElement extends Component {
 
-    render () {
-        return <div>Movie Element</div>
+    mouseEnter = () => {
+        this.props.updateSelectedMovie(this.props.movie.title);
     }
 
+    render () {
+        return (
+        <div  onMouseEnter={ this.mouseEnter } className="w-50 p-2">
+            <div className="border d-flex">
+                <img width="150" height="200" alt="film" src={this.props.movie.img}/>
+                <div className="felx-fill d-flex flex-column p-3">
+                    <h5>{this.props.movie.title}</h5>
+                    <hr className="w-100" />
+                    <span>{this.props.movie.details}</span>
+                </div>
+            </div>
+        </div>
+
+        );
+    }
 }
